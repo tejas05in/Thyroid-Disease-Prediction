8deb60757ee663393e4d9b014ca4b23cee643f37
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-02-02 20:22:08 +0530

    data validation updated

--- src/ThyroidProject/components/data_validation.py
+++ src/ThyroidProject/components/data_validation.py
@@ -23,7 +23,7 @@ class DataValidation:
             validation_status = None
 
             # Column names of the raw data
-            names = ["age", "sex", "on_thyroxine", "query_on_thyroxine", "on_antihyroid_meds", "sick", "pregnant", "thyroid_surgery", "I131_treatment", "query_hypothyroid", "query hyperthyroid", "lithium", "goitre",
+            names = ["age", "sex", "on_thyroxine", "query_on_thyroxine", "on_antihyroid_meds", "sick", "pregnant", "thyroid_surgery", "I131_treatment", "query_hypothyroid", "query_hyperthyroid", "lithium", "goitre",
                      "tumor", "hypopituitary", "psych", "TSH_measured", "TSH", "T3_measured", "T3", "TT4_measured", "TT4", "T4U_measured", "T4U", "FTI_measured", "FTI", "TBG_measured", "TBG", "referral_source", "target"]
 
             # Read the data from the unzip data directory
